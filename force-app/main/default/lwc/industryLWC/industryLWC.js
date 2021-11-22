b020e89cdc6c8f45c903d854ae2788c73ada5879
Maxim Turbal <maxim.turbal@vrpconsulting.com>
2021-11-22 12:42:17 +0200

    Update industryLWC.js

--- force-app/main/default/lwc/industryLWC/industryLWC.js
+++ force-app/main/default/lwc/industryLWC/industryLWC.js
@@ -1,5 +1,6 @@
 import { LightningElement, wire, track } from "lwc";
 import { getPicklistValues, getObjectInfo } from "lightning/uiObjectInfoApi";
+import getAccounts from "@salesforce/apex/AccController.getAccounts";
 import INDUSTRY from "@salesforce/schema/Account.Industry";
 import ACCOUNT_OBJECT from "@salesforce/schema/Account";
 
@@ -7,6 +8,7 @@ export default class PicklistDemo extends LightningElement {
     @track pickListvalues;
     @track error;
     @track values;
+    @track industryValue;
 
     @wire(getPicklistValues, {
         recordTypeId: "012000000000000AAA",
@@ -14,12 +16,12 @@ export default class PicklistDemo extends LightningElement {
     })
     wiredPickListValue({ data, error }) {
         if (data) {
-            console.log(` Picklist values are `, data.values);
+            // console.log(` Picklist values are `, data.values);
             this.pickListvalues = data.values;
             this.error = undefined;
         }
         if (error) {
-            console.log(` Error while fetching Picklist values  ${error}`);
+            // console.log(` Error while fetching Picklist values  ${error}`);
             this.error = error;
             this.pickListvalues = undefined;
         }
@@ -30,12 +32,31 @@ export default class PicklistDemo extends LightningElement {
     })
     wiredObject({ data, error }) {
         if (data) {
-            console.log(" Object iformation ", data);
+            // console.log(" Object iformation ", data.values);
             console.table(data);
         }
         if (error) {
             console.log(error);
         }
     }
-    handleChange() {}
+
+    @wire(getAccounts, { industryValue: "$handleValue" })
+    accounts({ error, data }) {
+        if (data) {
+            this.data = data;
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.data = undefined;
+        }
+    }
+    handleValue = undefined;
+
+    handleChange(data) {
+        console.log(
+            "🚀 ~ file: IndustryLWC.js ~ line 43 ~ PicklistDemo ~ handleChange ~ handleChange",
+            data.detail.value
+        );
+        this.handleValue = data.detail.value;
+    }
 }
